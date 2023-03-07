2a1027d450cfb073671786c85532fa0305eda46e
Oleg Riabtsev <lehaspost@gmail.com>
2023-03-07 23:47:30 +0100

    Amended list.py file

--- SkillFactory/Module_B2/list.py
+++ SkillFactory/Module_B2/list.py
@@ -18,6 +18,7 @@ print(letters)
 array_of_arrays = [
     [1, 2, 3],
     [4, 5, 6],
+    
     [7, 8, 9]
 ]
 print(array_of_arrays[2])
@@ -34,3 +35,10 @@ array[2][2] = 2**0.5
 print(array[0])
 print(array[1])
 print(array[2])
+
+new_list = ['apple', 'orange', 'grape']
+fruit = 'melon'
+if fruit in new_list:
+    print(f'Yes, {fruit} in the list ')
+else:
+    print(f'No, there is no {fruit} in the list')
